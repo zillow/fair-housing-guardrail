6a50ae76feb90663e71bde67c4f25effee08c9e9
Trevor Nogues <trevorn@zillowgroup.com>
2024-03-11 16:52:31 -0700

    rework pytest

--- tests/test_fair_housing_classification.py
+++ tests/test_fair_housing_classification.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest.mock as mock
 from io import StringIO
 
@@ -7,50 +8,76 @@ import pandas as pd
 from fair_housing_guardrail.utils.fair_housing_classification import (
     FairHousingGuardrailClassification,
 )
-from fair_housing_guardrail.utils.helper import load_config, load_tokenizer
+from fair_housing_guardrail.utils.helper import load_config, load_dataset, load_tokenizer
 
 current_path = os.getcwd()
-CONFIG_FILE_PATH = os.path.join(current_path, "examples/configs/test-config.yaml")
-config = load_config(CONFIG_FILE_PATH)
-config["input_model"]["model_dir"] = os.path.join(current_path, "examples/test_model/")
-config["input_data"]["stop_list_path"] = os.path.join(
+
+# Setup train
+TRAIN_CONFIG_FILE_PATH = os.path.join(current_path, "examples/configs/train-config.yaml")
+train_config = load_config(TRAIN_CONFIG_FILE_PATH)
+train_config["output_data"]["output_dir"] = os.path.join(current_path, "tmp/")
+train_config["output_data"]["model_output_dir"] = os.path.join(current_path, "tmp/model/")
+train_config["input_data"]["stop_list_path"] = os.path.join(
     current_path, "examples/datasets/sample_stoplist.txt"
 )
-config["input_data"]["input_data_path"] = os.path.join(
+train_config["input_data"]["input_data_path"] = os.path.join(
     current_path, "examples/datasets/sample_data.jsonl"
 )
+
+# Setup test
+TEST_CONFIG_FILE_PATH = os.path.join(current_path, "examples/configs/test-config.yaml")
+test_config = load_config(TEST_CONFIG_FILE_PATH)
+test_config["input_model"]["model_dir"] = os.path.join(current_path, "tmp/model/")
+test_config["input_data"]["stop_list_path"] = os.path.join(
+    current_path, "examples/datasets/sample_stoplist.txt"
+)
+test_config["input_data"]["input_data_path"] = os.path.join(
+    current_path, "examples/datasets/sample_data.jsonl"
+)
+# Setup tokenizer
 tokenizer = load_tokenizer()
 
 
-@mock.patch("fair_housing_guardrail.utils.fair_housing_classification.SigmoidTrainer")
-def test_train(mock_trainer_class):
-    instance = FairHousingGuardrailClassification(config, tokenizer)
+def test_train_and_predict():
+    with open(train_config["input_data"]["input_data_path"], "r") as file:
+        json_data = file.read()
+        train_dataset, test_dataset = load_dataset(train_config, tokenizer)
 
-    mock_trainer = mock.Mock()
-    mock_trainer.state.log_history = [
-        {"loss": 0.1, "step": 1},
-        {"eval_loss": 0.2, "step": 2},
-        {"loss": 0.3, "step": 3},
-        {"eval_loss": 0.4, "step": 4},
-    ]
-    mock_trainer_class.return_value = mock_trainer
+    instance = FairHousingGuardrailClassification(
+        train_config, tokenizer, train_data=train_dataset, test_data=test_dataset
+    )
+    temp_dir = os.path.join(current_path, "tmp/")
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
 
+    # Test train
     train_loss, eval_loss = instance.train()
+    print(train_loss, eval_loss)
 
+    instance.trainer.save_model()
+    instance.model.save_pretrained(train_config["output_data"]["model_output_dir"])
+
+    config_path = os.path.join(temp_dir, "config.json")
+
+    assert os.path.exists(config_path)
     assert isinstance(train_loss, list)
     assert isinstance(eval_loss, list)
-
     assert len(train_loss) > 0
     assert len(eval_loss) > 0
 
+    # Test predict
+    with open(test_config["input_data"]["input_data_path"], "r") as file:
+        json_data = file.read()
+        predict_data = pd.read_json(StringIO(json_data), orient="records", lines=True)
 
-with open(config["input_data"]["input_data_path"], "r") as file:
-    json_data = file.read()
-    test_data = pd.read_json(StringIO(json_data), orient="records", lines=True)
+    run_predict(predict_data)
+
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
 
 
-def test_predict():
-    instance = FairHousingGuardrailClassification(config, tokenizer, None, test_data)
+def run_predict(predict_data):
+    instance = FairHousingGuardrailClassification(test_config, tokenizer, None, predict_data)
     predictions = instance.predict()
 
     assert len(predictions) == 13
